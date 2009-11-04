74042471696685cb6db1d7d8509a4b676a204f9c
Tony Arber <T.D.Arber@warwick.ac.uk>
2009-11-04 15:54:48 +0000

    Moved get_energy to neutral.f90
    git-svn-id: http://ccpforge.cse.rl.ac.uk/svn/lare3d/trunk@59 7344f857-124b-0410-9404-94b5de021bc2

--- src/core/eos.F90
+++ src/core/eos.F90
@@ -8,9 +8,7 @@ MODULE EOS
 CONTAINS
 
   ! This module contains all the information about the equations of state
-  ! used by LARE. These are expressed in normalised units except for
-  ! get_energy which is only used in the initial conditions and MUST be
-  ! in SI units using the define constants constants
+  ! used by LARE. 
 
   SUBROUTINE get_pressure(rho_in, en_in, m_in, ix, iy, iz, p)
     REAL(num), INTENT(IN) :: rho_in, en_in ! input energy & density
@@ -94,49 +92,5 @@ CONTAINS
 
   END SUBROUTINE get_cs
 
-
-
-  SUBROUTINE get_energy(rho_in, temp_in, m_in, ix, iy, iz, en_out)
-
-    REAL(num), INTENT(IN) :: rho_in, temp_in
-    INTEGER, INTENT(IN) :: m_in, ix, iy, iz
-    REAL(num), INTENT(OUT) :: en_out
-    REAL(num) :: xi_local, bof, r
-
-    IF (m_in .EQ. EOS_IDEAL) THEN
-      en_out = temp_in * kb / ((gamma - 1.0_num) * mbar / 2.0_num)
-      RETURN
-    END IF
-
-    IF (m_in .EQ. EOS_PI) THEN
-      ! Since we can't guarantee that the ionisation fraction already
-      ! calculated is correct here, calculate it straight from the temperature
-      bof = tr_bar / (f_bar * SQRT(temp_in)) &
-          * EXP((0.25_num * (tr_bar * temp_in - 1.0_num) + 1.0_num) &
-          * T_bar / temp_in)
-      r = 0.5_num * (-1.0_num + SQRT(1.0_num + r_bar * rho_in * bof))
-      xi_local = r / (1.0_num + r)
-
-      en_out = (kb * temp_in * (2.0_num - xi_local)) &
-          / (MBAR * (gamma - 1.0_num))
-      RETURN
-    END IF
-
-    IF (m_in .EQ. EOS_ION) THEN
-      ! Since we can't guarantee that the ionisation fraction already
-      ! calculated is correct here, calculate it straight from the temperature
-      bof = tr_bar / (f_bar * SQRT(temp_in)) &
-          * EXP((0.25_num * (tr_bar * temp_in - 1.0_num) + 1.0_num) &
-          * T_bar / temp_in)
-      r = 0.5_num * (-1.0_num + SQRT(1.0_num + r_bar * rho_in * bof))
-      xi_local = r / (1.0_num + r)
-
-      en_out = (kb * temp_in * (2.0_num - xi_local) &
-          + (1.0_num - xi_local) * ionise_pot * (gamma - 1.0_num)) &
-          / (MBAR * (gamma - 1.0_num))
-      RETURN
-    END IF
-
-  END SUBROUTINE get_energy
 
 END MODULE EOS
